f6d93b0d5807c527ace5c60766df08f31ee2e4b9
吴磊 <wuleiaty@users.noreply.github.com>
2017-10-25 21:59:44 +0800

    10/25

--- src/main/java/com/wulei/Controller/FileController.java
+++ src/main/java/com/wulei/Controller/FileController.java
@@ -2,12 +2,10 @@ package com.wulei.Controller;
 
 import com.wulei.Beans.File;
 import com.wulei.Service.FileService;
+import com.wulei.Service.XlsxResolveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
@@ -18,6 +16,8 @@ import java.util.List;
 public class FileController {
     @Autowired
     FileService fileService;
+    @Autowired
+    XlsxResolveService xlsxResolveService;
 
     @RequestMapping(value = "/uploadfile", method = RequestMethod.POST)
     public @ResponseBody String uploadFile(@RequestParam("file") MultipartFile file, HttpSession session){
@@ -27,6 +27,35 @@ public class FileController {
     @RequestMapping(value = "/getfilelist", method = RequestMethod.GET)
     public @ResponseBody List<File> getFileList(HttpSession session){
         return fileService.getUserFileList(session);
+    }
+
+    @RequestMapping(value = "/getContent/{filename}/{sheetname}", method = RequestMethod.GET)
+    public @ResponseBody List<Object> getContent(
+            @PathVariable("filename") String fileName,
+            @PathVariable("sheetname") String sheetName,
+            HttpSession session){
+        List<File> fileList = fileService.getUserFileList(session);
+        for(File file : fileList){
+            if(file.getFileName().equals(fileName)){
+                xlsxResolveService.initFile(file.getFilePath());
+                if(xlsxResolveService.getSheetList().contains(sheetName)){
+                    return xlsxResolveService.getContentBySheetName(sheetName);
+                }
+            }
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/getFileSheetList/{filename}", method = RequestMethod.GET)
+    public @ResponseBody List<String> getFileSheetList(@PathVariable("filename") String fileName, HttpSession session){
+        List<File> fileList = fileService.getUserFileList(session);
+        for(File file : fileList){
+            if(file.getFileName().equals(fileName)){
+                xlsxResolveService.initFile(file.getFilePath());
+                return xlsxResolveService.getSheetList();
+            }
+        }
+        return null;
     }
 
 }
